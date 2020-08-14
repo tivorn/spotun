26c43beb2f8c524cdca6192e3c98fd156a204176
Victor Navarro <55517000+tivorn@users.noreply.github.com>
2020-08-13 21:00:36 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function showAreas() {
+  document.getElementById("navbar__item--dropdown").classList.toggle("active");
+}
+
+document.getElementById("js-navbar__item").addEventListener("click", showAreas);
